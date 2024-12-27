48e68658d5dc83fe31f1ce61f3fcf4ad86c83437
Eelis Rossi <eelis.rossi@gmail.com>
2024-12-27 15:04:18 +0200

    day 5 part 2 done

--- day05.go
+++ day05.go
@@ -5,12 +5,10 @@ import (
 	"strings"
 )
 
-func day05_1(input string) int {
-	lines := strings.Split(strings.TrimSpace(input), "\n")
+func getRulesAndUpdates(lines []string) ([][]int, [][]int) {
 	rules := make([][]int, 0)
 	updates := make([][]int, 0)
 	gotLines := false
-	output := 0
 
 	for _, line := range lines {
 		if !gotLines && len(line) != 0 {
@@ -23,11 +21,11 @@ func day05_1(input string) int {
 			rules = append(rules, rule)
 		}
 		if gotLines {
-            updateSplit := strings.Split(line, ",")
-            update := make([]int, len(updateSplit))
-            for i, u := range updateSplit {
-               update[i], _ = strconv.Atoi(u)
-            }
+			updateSplit := strings.Split(line, ",")
+			update := make([]int, len(updateSplit))
+			for i, u := range updateSplit {
+				update[i], _ = strconv.Atoi(u)
+			}
 			updates = append(updates, update)
 		}
 		if len(line) == 0 && !gotLines {
@@ -35,39 +33,132 @@ func day05_1(input string) int {
 		}
 	}
 
-    for _, update := range updates {
-        isSafe := true
-        for index, val := range update {
+	return rules, updates
+}
 
-            for _, rule := range rules {
-                if val == rule[0] {
-                    for i := 0; i < index; i++ {
-                        if update[i] == rule[1] {
-                            isSafe = false
-                            break
-                        }
-                    }
-                }
-            }
-            if !isSafe {
-                break
-            }
-        }
-        if isSafe {
-            middleIndex := len(update) / 2
-            middlePage := update[middleIndex]
-            output += middlePage
-        }
-    }
+func checkUpdate(update []int, rules [][]int) bool {
+	for index, val := range update {
+		for _, rule := range rules {
+			if val == rule[0] {
+				if !checkRule(update, rule, index) {
+					return false
+				}
+			}
+		}
+	}
 
+	return true
+}
 
+func checkRule(update []int, rule []int, index int) bool {
+	for i := 0; i < index; i++ {
+		if update[i] == rule[1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func day05_1(input string) int {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	output := 0
+
+	rules, updates := getRulesAndUpdates(lines)
+
+	for _, update := range updates {
+		isSafe := true
+		isSafe = checkUpdate(update, rules)
+		if isSafe {
+			middleIndex := len(update) / 2
+			middlePage := update[middleIndex]
+			output += middlePage
+		}
+	}
 
 	return output
 }
 
 func day05_2(input string) int {
-	// lines := strings.Split(strings.TrimSpace(input), "\n")
+	lines := strings.Split(strings.TrimSpace(input), "\n")
 	output := 0
 
+	rules, updates := getRulesAndUpdates(lines)
+
+	for _, update := range updates {
+		isSafe := true
+		isSafe = checkUpdate(update, rules)
+		if !isSafe {
+			correct := fixOrdering(update, rules)
+			middleIndex := len(correct) / 2
+			middlePage := correct[middleIndex]
+			output += middlePage
+		}
+	}
+
 	return output
+}
+
+func fixOrdering(update []int, rules [][]int) []int {
+	correctUpdate := make([]int, len(update))
+    newRules := getRightRules(update, rules)
+
+    for i, value := range update {
+        if correctUpdate[i] == 0 {
+            correctUpdate[i] = value
+        }
+        for _, rule := range newRules {
+            before := rule[0]
+            after := rule[1]
+            if value == before {
+                for index := 0; index < i; index++ {
+                    if correctUpdate[index] == after {
+                        a := correctUpdate[index]
+                        b := correctUpdate[i]
+                        correctUpdate[index] = b
+                        correctUpdate[i] = a
+                    }
+                }
+            }
+        }
+    }
+
+    safe := checkUpdate(correctUpdate, rules)
+    if !safe {
+        correctUpdate = fixOrdering(correctUpdate, rules)
+    }
+
+	return correctUpdate
+}
+
+func getRightRules(update []int, rules [][]int) [][]int {
+	tempRules := make([][]int, 0)
+	newRules := make([][]int, 0)
+
+	for _, rule := range rules {
+		for _, u := range update {
+			if rule[0] == u || rule[1] == u {
+				tempRules = append(tempRules, rule)
+			}
+		}
+	}
+
+    for _, r := range tempRules {
+        exists := false
+        if len(newRules) == 0 {
+            newRules = append(newRules, r)
+        }
+        if len(newRules) > 0 {
+            for _, nr := range newRules {
+                if r[0] == nr[0] && r[1] == nr[1] {
+                    exists = true
+                }
+            }
+        }
+        if !exists {
+            newRules = append(newRules, r)
+        }
+    }
+
+    return newRules
 }
